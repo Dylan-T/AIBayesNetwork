90985ee82436ad71b162545effef7d6c99049135
thomsodyla1 <thomsodyla1@myvuw.ac.nz>
2019-05-20 16:32:27 +1200

    tinkered with algorithm still not working

--- p2.py
+++ p2.py
@@ -37,7 +37,7 @@ for i in range(0, len(testData)):
 # ...
 
 # Create table for n features
-table = [[1, 1] for n in range(len(trainData[0])*2 - 1)]
+table = [[1, 1, 2] for n in range(len(trainData[0])*2 - 1)]
 
 # Count occurrences
 for sample in trainData:
@@ -45,30 +45,47 @@ for sample in trainData:
     while i < len(table) - 1:
         i = i + sample[int(i/2)]
         table[i][sample[len(sample)-1]] = table[i][sample[len(sample)-1]] + 1
+        table[i][2] = table[i][2] + 1
         i = i + 2 - (i % 2)  # iterate to next feature
     table[len(table)-1][sample[len(sample)-1]] = table[len(table)-1][sample[len(sample)-1]] + 1  # add to class count
+    table[len(table) - 1][2] = table[len(table) - 1][2] + 1
+print("====Count====")
+print(table)
 
+# Create probability table
+pTable = [[1, 1, 1] for n in range(len(trainData[0])*2 - 1)]
+for i in range(0, len(pTable)-1):
+    for clazz in range(3):
+        pTable[i][clazz] = table[i][clazz] / table[len(table)-1][clazz]
+pTable[len(pTable)-1][0] = table[len(table)-1][0] / table[len(table)-1][2]
+pTable[len(pTable)-1][1] = table[len(table)-1][1] / table[len(table)-1][2]
+
+print("====Probability====")
+print(pTable)
+
+print("====Classify====")
 # Classify:
-
 for sample in testData:
-    # calculate P(class1|data)  == P(data|class1)*P
-    p1 = 1
+    # calculate P(class|data)  == (P(data|class)*P(class)) / p(data)
+    p1 = 1  # P(data|class1)
+    p2 = 1  # P(data|class2)
+    pTotal = 1  # P(data)
     i = 0
     while i < len(table) - 1:
         i = i + sample[int(i / 2)]
-        p1 = p1 * table[i][0] / table[len(table)-1][0]
+        p1 = p1 * pTable[i][0]
+        p2 = p2 * pTable[i][1]
+        pTotal = pTotal * pTable[i][2]
         i = i + 2 - (i % 2)  # iterate to next feature
-    p1 = p1*(table[len(table)-1][0]/(table[len(table)-1][0]+table[len(table)-1][1]))
+    p1 = (p1*pTable[len(pTable)-1][0]) / pTotal
+    p2 = (p2*pTable[len(pTable)-1][1]) / pTotal
 
-    # calculate P(class1|data)  == P(data|class1)*P
-    p2 = 1
-    i = 0
-    while i < len(table) - 1:
-        i = i + sample[int(i / 2)]
-        p2 = p2 * table[i][1] / table[len(table)-1][1]
-        i = i + 2 - (i % 2)  # iterate to next feature
-    p2 = p2*(table[len(table)-1][1]/(table[len(table)-1][0]+table[len(table)-1][1]))
-
-    # return class with higher P
     print(p1)
     print(p2)
+    print(p1+p2)
+    if p1>p2 : print("spam")
+    else: print("not spam")
+    print("===")
+
+    # return class with higher P
+
