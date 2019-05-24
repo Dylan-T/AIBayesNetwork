612d10c36ff508d6d4f2cc8f7cc3ad79d9eb77d6
thomsodyla1 <thomsodyla1@myvuw.ac.nz>
2019-05-24 16:48:10 +1200

    ..

--- p2.py
+++ p2.py
@@ -12,50 +12,73 @@ def load_data(filename):
 
 
 def create_table(data):
+    n_rows = len(data[0])*2 - 1
+
     # Create table for n features
-    table = [[1, 1, 2] for n in range(len(data[0])*2 - 1)]
+    table = [[1, 1] for n in range(n_rows-1)]
+    table.append([2, 2])
     # Count occurrences
     for sample in data:
+        s_class = sample[len(sample)-1]
         i = 0
-        while i < len(table) - 1:
+        while i < n_rows - 1:
             i = i + sample[int(i/2)]
-            table[i][sample[len(sample)-1]] = table[i][sample[len(sample)-1]] + 1
-            table[i][2] = table[i][2] + 1
+            table[i][s_class] = table[i][s_class] + 1
             i = i + 2 - (i % 2)  # iterate to next feature
-        table[len(table)-1][sample[len(sample)-1]] = table[len(table)-1][sample[len(sample)-1]] + 1 # add to class count
-        table[len(table) - 1][2] = table[len(table) - 1][2] + 1
+        table[n_rows-1][s_class] = table[n_rows-1][s_class] + 1  # add to class count
     return table
 
 
 def create_prob_table(table):
     # Create probability table
-    pTable = [[1, 1, 1] for n in range(len(table))]
-    for i in range(0, len(pTable)-1):
-        for clazz in range(3):
-            pTable[i][clazz] = table[i][clazz] / table[len(table)-1][clazz]
-    pTable[len(pTable)-1][0] = table[len(table)-1][0] / table[len(table)-1][2]
-    pTable[len(pTable)-1][1] = table[len(table)-1][1] / table[len(table)-1][2]
+    n_rows = len(table)
+    pTable = [[1, 1] for n in range(n_rows)]
+    for i in range(0, n_rows-1):
+        for clazz in range(2):
+            pTable[i][clazz] = table[i][clazz] / table[n_rows-1][clazz]
+    pTable[n_rows-1][0] = table[n_rows-1][0] / (table[n_rows-1][0] + table[n_rows-1][1])
+    pTable[n_rows-1][1] = table[n_rows-1][1] / (table[n_rows-1][0] + table[n_rows-1][1])
     return pTable
 
 
 def classify(sample, pTable):
-    # calculate P(class|data)  == (P(data|class)*P(class)) / p(data)
-    p1 = 1  # P(data|class1)
-    p2 = 1  # P(data|class2)
+    # calculate P(class|data)  == (P(data|class)*P(class))
+    n_rows = len(pTable)
+    p0 = pTable[n_rows-1][0]
+    p1 = 1 - p0
     i = 0
-    while i < len(pTable) - 1:
+    while i < n_rows - 1:
         i = i + sample[int(i / 2)]
-        p1 = p1 * pTable[i][0]
-        p2 = p2 * pTable[i][1]
+        p0 = p0 * pTable[i][0]
+        p1 = p1 * pTable[i][1]
         i = i + 2 - (i % 2)  # iterate to next feature
-    p1 = (p1*pTable[len(pTable)-1][0])
-    p2 = (p2*pTable[len(pTable)-1][1])
-
     # return class with higher P
-    if p1 > p2:
+    print("Instance " + str(c) + ":")
+    print("Input: " + str(sample))
+    print("P(S|D): " + str(p1))
+    print("P(!S|D): " + str(p0))
+    if p0 > p1:
+        print("Class: Not Spam")
+    else:
+        print("Class: Spam")
+    print()
+    if p0 > p1:
         return 0
     else:
         return 1
+
+
+def log_tables(table, pTable):
+    file = open("log.txt", "w")
+    file.write("Occurrence table: \n")
+    for i in range(len(table)):
+        file.write(str(table[i]))
+        file.write("\n")
+
+    file.write("Probability table:\n")
+    for i in range(len(pTable)):
+        file.write(str(pTable[i]))
+        file.write("\n")
 
 
 # Take inputs
@@ -69,12 +92,14 @@ else:
 table = create_table(train)
 pTable = create_prob_table(table)
 
+log_tables(table, pTable)
+global c
+c = 1
 results = []
 for sample in test:
     results.append(classify(sample, pTable))
+    c += 1
 
 print(results)
-open("sampleoutput.txt", "w")
-# 1 - spam
-# 0 - non spam
-# JJ results [1, 0, 0, 1, 0, 0, 1, 0, 0, 1]
+file = open("sampleoutput.txt", "w")
+file.write(str(results))
